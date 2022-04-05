074be56f24be8eaaa6e19622b5b68b4c684ac62d
ik-top-digital <top.digital.ik@gmail.com>
2022-04-05 09:38:16 +0300

    Initial Header in accroding with design first step

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,31 @@ const gulp = require('gulp');
 const themeKit = require('@shopify/themekit');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+var supported = [
+    'last 2 versions',
+    'safari >= 8',
+    'ie >= 10',
+    'ff >= 20',
+    'ios 6',
+    'android 4'
+  ];
 
 gulp.task('sass', function(){
     return gulp.src('scss/global.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano({
+        autoprefixer: {browsers: supported, add: true},
+        discardComments: {
+          removeAll: true
+        }
+      }))  
     .pipe(clean({compatibility: 'ie11'}))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets'))
 });
 
